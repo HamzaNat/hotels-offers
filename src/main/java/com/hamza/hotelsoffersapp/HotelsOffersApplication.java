74c8d10c2b5eb5973ec20a2196f7aa8f1f9a43aa
Hamza Al Natsheh <hamza.natsheh@outlook.com>
2018-03-11 02:41:22 +0200

    make project deployable to a servlet container

--- src/main/java/com/hamza/hotelsoffersapp/HotelsOffersApplication.java
+++ src/main/java/com/hamza/hotelsoffersapp/HotelsOffersApplication.java
@@ -2,9 +2,16 @@ package com.hamza.hotelsoffersapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HotelsOffersApplication {
+public class HotelsOffersApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HotelsOffersApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HotelsOffersApplication.class, args);
